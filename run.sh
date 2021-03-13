efe1fb9aaed9dabd796fbe19b93caedfe1167707
tf2autobot-admin <tf2autobot@gmail.com>
2021-03-13 05:05:42 +0100

    🎨⛔ add custom error pages

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 
 cp ./index.html /data/www/autobot.tf
 cp -r ./assets /data/www/autobot.tf
+cp -r ./assets /data/www/api.autobot.tf
+cp -r ./assets /data/www/wiki.autobot.tf
+cp -r ./errorPages /data/www/
 
 echo "Done copy!"
