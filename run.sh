097f0b399937c144e1fee78acdd0f7586816b309
idinium96 <47635037+idinium96@users.noreply.github.com>
2021-06-24 09:27:14 +0800

    🔥 delete script

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cp ./index.html /data/www/autobot.tf
-cp -r ./assets /data/www/autobot.tf
-cp -r ./assets /data/www/api.autobot.tf
-cp -r ./assets /data/www/wiki.autobot.tf
-cp -r ./errorPages /data/www/
-
-echo "Done copy!"
