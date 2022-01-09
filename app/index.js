1115cb7e63b2cc7bc8d44bf3779232fb530182ba
idinium96 <47635037+idinium96@users.noreply.github.com>
2022-01-09 21:32:13 +0800

    ✨ get correct images

--- app/index.js
+++ app/index.js
@@ -11,6 +11,8 @@ const init = require('./schema');
 const express = require('express');
 
 const SKU = require('@tf2autobot/tf2-sku');
+const generateBptfUrl = require('../utils/generateBptfUrl');
+const getImage = require('../utils/getImage');
 
 const ejs = require('ejs');
 
@@ -62,7 +64,7 @@ init()
             res.render('items/index', {
                 sku: req.params.sku,
                 name: itemName,
-                image: baseItemData.image_url_large,
+                image: getImage(item, itemName, baseItemData),
                 description: baseItemData.item_description,
                 bptfUrl: generateBptfUrl(schema, item),
             });
@@ -75,111 +77,6 @@ init()
     .catch((err) => {
         throw new Error(err);
     });
-
-function generateBptfUrl(schema, item) {
-    const base = 'https://backpack.tf/stats/';
-
-    const name = schema.getName(
-        {
-            defindex: item.defindex,
-            quality: 6,
-            festive: item.festive,
-            killstreak: item.killstreak,
-            australium: item.australium,
-            target: item.target,
-            paintkit: item.paintkit,
-            wear: item.wear,
-        },
-        false
-    );
-
-    const nameLowered = name.toLowerCase();
-
-    const isUnusualifier =
-        nameLowered.includes('unusualifier') && item.target !== null;
-
-    const isStrangifierChemistrySet =
-        nameLowered.includes('chemistry set') &&
-        item.target !== null &&
-        item.output !== null &&
-        item.outputQuality !== null;
-
-    const isCollectorsChemistrySet =
-        nameLowered.includes('chemistry set') &&
-        item.target === null &&
-        item.output !== null &&
-        item.outputQuality !== null;
-
-    const isStrangifier =
-        nameLowered.includes('strangifier') && item.target !== null;
-
-    const isFabricator =
-        nameLowered.includes('fabricator') &&
-        item.target !== null &&
-        item.output !== null &&
-        item.outputQuality !== null;
-    const isGenericFabricator =
-        nameLowered.includes('fabricator') &&
-        item.target === null &&
-        item.output !== null &&
-        item.outputQuality !== null;
-
-    const isKillstreakKit =
-        nameLowered.includes('kit') &&
-        item.killstreak !== 0 &&
-        item.target !== null;
-
-    const itemName = isStrangifier
-        ? 'Strangifier'
-        : isFabricator
-        ? item.killstreak === 2
-            ? 'Specialized Killstreak Fabricator'
-            : 'Professional Killstreak Fabricator'
-        : isKillstreakKit
-        ? item.killstreak === 1
-            ? 'Killstreak Kit'
-            : item.killstreak === 2
-            ? 'Specialized Killstreak Kit'
-            : 'Professional Killstreak Kit'
-        : name.includes('Haunted Metal Scrap') ||
-          name.includes("Horseless Headless Horsemann's Headtaker")
-        ? name.replace('Unique ', '')
-        : name;
-
-    const quality =
-        (item.quality2 !== null
-            ? schema.getQualityById(item.quality2) + ' '
-            : '') + schema.getQualityById(item.quality);
-
-    const tradable = `${!item.tradable ? 'Non-' : ''}Tradable`;
-    const craftable = `${!item.craftable ? 'Non-' : ''}Craftable`;
-
-    const priceindex =
-        item.effect !== null
-            ? item.effect
-            : item.crateseries !== null
-            ? item.crateseries
-            : isUnusualifier || isStrangifier
-            ? item.target
-            : isFabricator
-            ? `${item.output}-${item.outputQuality}-${item.target}`
-            : isKillstreakKit
-            ? `${item.killstreak}-${item.target}`
-            : isStrangifierChemistrySet
-            ? `${item.target}-${item.outputQuality}-${item.output}`
-            : isCollectorsChemistrySet
-            ? `${item.output}-${item.outputQuality}`
-            : isGenericFabricator
-            ? `${item.output}-${item.outputQuality}-0`
-            : undefined;
-
-    return (
-        base +
-        `${quality}/${itemName}/${tradable}/${craftable}${
-            priceindex ? '/' + priceindex : ''
-        }`
-    );
-}
 
 const ON_DEATH = require('death');
 const inspect = require('util');
