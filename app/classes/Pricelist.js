f30793e1f203c68b9b9c4caa7081d8bde5678a73
idinium96 <47635037+idinium96@users.noreply.github.com>
2022-04-25 07:48:50 +0800

    😂

--- app/classes/Pricelist.js
+++ app/classes/Pricelist.js
@@ -88,6 +88,7 @@ class Pricelist {
 
             this.pricer.getPricelist().then((pricelist) => {
                 this.setPricelist(pricelist.items);
+                this.setupPricerHealthCheck();
 
                 this.pricer.bindHandlePriceEvent(this.boundHandlePriceChange);
 
