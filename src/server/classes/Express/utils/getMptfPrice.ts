cd051134496676904787425491363bb218c4a992
IdiNium <47635037+idinium96@users.noreply.github.com>
2022-06-04 06:02:24 +0800

    🔨 forgot to upgrade to axios

--- src/server/classes/Express/utils/getMptfPrice.ts
+++ src/server/classes/Express/utils/getMptfPrice.ts
@@ -1,4 +1,4 @@
-import request from 'request-retry-dayjs';
+import axios from 'axios';
 
 interface GetMptfPrice {
     prices?: {
@@ -13,25 +13,20 @@ interface GetMptfPrice {
 
 export default function getMptfPrice(bptfQuery: string): Promise<string> {
     return new Promise((resolve, reject) => {
-        const base = 'https://api.backpack.tf/item/get_third_party_prices/';
-        const options = {
+        void axios({
             method: 'GET',
-            url: base + bptfQuery,
+            url: '/' + bptfQuery,
+            baseURL: 'https://api.backpack.tf/item/get_third_party_prices',
             headers: {
                 'User-Agent': 'autobot.tf@' + process.env.SERVER_VERSION
             },
-            json: true,
-            gzip: true,
-            maxAttempts: 1,
-            timeout: 2500 // set timeout to only 2.5 seconds, default was 10 seconds
-        };
-
-        void request(options, (err, response, body) => {
-            if (err) {
+            timeout: 2500
+        })
+            .then(response => {
+                return resolve((response.data as GetMptfPrice).prices?.mp?.lowest_price);
+            })
+            .catch(err => {
                 return reject(err);
-            }
-
-            return resolve((body as GetMptfPrice).prices?.mp?.lowest_price);
-        });
+            });
     });
 }
