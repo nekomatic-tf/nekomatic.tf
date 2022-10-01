ec70eaf3e5ab8abd949e5e8554406a059a5f57c3
IdiNium <47635037+idinium96@users.noreply.github.com>
2022-10-01 21:03:18 +0800

    🔨 5 requests/second

--- src/server/classes/Express/Middlewares/rateLimiter.ts
+++ src/server/classes/Express/Middlewares/rateLimiter.ts
@@ -5,8 +5,8 @@ import rateLimit from 'express-rate-limit';
 
 export const rateLimiterUsingThirdParty = rateLimit({
     windowMs: 1000,
-    max: 1,
-    message: 'You have exceeded the 1 request/second limit!',
+    max: 5,
+    message: 'You have exceeded the 5 requests/second limit!',
     standardHeaders: true,
     legacyHeaders: false
 });
