3b642a5e51ce42fd7ca196fceb5d2cd71078a82a
suchmax <otrusinka98@gmail.com>
2023-10-22 18:35:45 +0200

    rename crate.tf to cases.tf

--- src/server/lib/tools/getCasestfCrateList.ts
+++ src/server/lib/tools/getCasestfCrateList.ts
@@ -1,23 +1,23 @@
 import log from '../logger';
 import PricesTfApi from '../pricer/pricestf/prices-tf-api';
 
-export default async function getCratetfCratesList(): Promise<string[]> {
+export default async function getCasestfCratesList(): Promise<string[]> {
     try {
-        log.info('Getting crate list from crate.tf...');
+        log.info('Getting crate list from cases.tf...');
         const response = await PricesTfApi.apiRequest(
             'GET',
             '/backend',
             { action: 'getcratelist' },
             null,
             null,
-            'https://backend.crate.tf'
+            'https://backend.cases.tf'
         );
 
         // eslint-disable-next-line
         // @ts-ignore
         return response ?? [];
     } catch (e) {
-        log.error(`Error fetching cratelist from crate.tf: ${String(e)}`);
+        log.error(`Error fetching cratelist from cases.tf: ${String(e)}`);
         return [];
     }
 }
